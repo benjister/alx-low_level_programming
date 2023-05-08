35e1d85f5b3bf395fcc6445c3c2a844139303d87
benjister <122888104+benjister@users.noreply.github.com>
2023-05-08 17:16:48 -0400

    Update 0-read_textfile.c
    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,49 @@
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: the name of the file to read
- * @letters: the number of letters to read and print
+ * read_textfile - reads a text file and prints it to the POSIX stdout
+ * @filename: pointer to the name of the file to be read
+ * @letters: number of letters to read and print
  *
- * Return: the number of letters read and printed, or 0 if an error occurred
+ * Return: number of letters read and printed, or 0 if an error occurred
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, ret;
-    char *buf;
+	int fd;
+	ssize_t nread, nwritten;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    buf = malloc(letters * sizeof(char));
-    if (buf == NULL)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    ret = read(fd, buf, letters);
-    if (ret == -1)
-    {
-        free(buf);
-        return (0);
-    }
+	nread = read(fd, buffer, letters);
+	if (nread == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    ret = write(STDOUT_FILENO, buf, ret);
-    if (ret == -1 || ret != letters)
-    {
-        free(buf);
-        return (0);
-    }
+	nwritten = write(STDOUT_FILENO, buffer, nread);
+	if (nwritten == -1 || nwritten != nread)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    free(buf);
-    close(fd);
-
-    return (ret);
+	free(buffer);
+	close(fd);
+	return (nwritten);
 }
