3f17fbc3ea974414b55e01b57571bd4c5e289039
benjister <122888104+benjister@users.noreply.github.com>
2023-05-08 17:24:53 -0400

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,37 @@
 #include "main.h"
+
 /**
- * create_file -creates an array of chars, and initializes
- *
- * @text_content: is a NULL terminated string to write to the file
- * @filename: is the name of the file to create
+ * create_file - creates a file with the given name and writes the given
+ * text content to it
+ * @filename: the name of the file to create
+ * @text_content: the NULL-terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int fd, i;
+	ssize_t n;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
-
-	if (o == -1 || w == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
 
-	close(o);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+		n = write(fd, text_content, i);
+		if (n == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 
+	close(fd);
 	return (1);
 }
